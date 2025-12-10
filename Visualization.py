477af4c6539445f7b548e4dd8e6959c24a63cc4a
yayayida <heeoneone@gmail.com>
2025-12-09 21:24:17 -0500

    Enhance Visualize function with volume label feature
    Added an option to display traffic volume labels on the map for both car and public transit traffic when above a specified threshold.

--- Visualization.py
+++ Visualization.py
@@ -2,7 +2,8 @@ import pandas
 import geopandas
 import numpy as np
 import folium
-def Visualize(G_car, G_pt, zones, title=None):
+from folium.features import DivIcon
+def Visualize(G_car, G_pt, zones, title=None, show_text_threshold=None):
     """
     Function plots the chloropeth map with zones as the base and lines from the network graph. 
     The width of the line is controled using the "Volume" in the traffic.
@@ -25,24 +26,52 @@ def Visualize(G_car, G_pt, zones, title=None):
     carFg = folium.FeatureGroup("Car Traffic")
     for zone in G_car.edges:
         volume = G_car.edges[zone]['volume']
-        coordinates = [zones.loc[zone[0], 'centroid'], zones.loc[zone[1], 'centroid']]
+        p1 = zones.loc[zone[0], 'centroid']
+        p2 = zones.loc[zone[1], 'centroid']
+        coordinates = [p1, p2]
         folium.PolyLine(locations=coordinates,
                         tooltip=f"Car Volume:{volume:.3f}", 
                         weight=volume/5000,
                         opacity=0.5,
                         color = 'red').add_to(carFg)
+        if show_text_threshold and volume > show_text_threshold:
+            mid_lat = (p1[0] + p2[0]) / 2
+            mid_lon = (p1[1] + p2[1]) / 2
+            
+            folium.map.Marker(
+                [mid_lat, mid_lon],
+                icon=DivIcon(
+                    icon_size=(150,36),
+                    icon_anchor=(0,0),
+                    html=f'<div style="font-size: 10pt; font-weight: bold; color: darkred; background-color: rgba(255,255,255,0.6); padding: 2px; border-radius: 3px; display: inline-block;">{int(volume)}</div>'
+                )
+            ).add_to(carFg)
     carFg.add_to(m)
 
     # Plotting lines for Pubic Transit using pt_network_graph
     ptFg = folium.FeatureGroup("Public Transport Traffic")
     for zone in G_pt.edges:
         volume = G_pt.edges[zone]['volume']
-        coordinates = [zones.loc[zone[0], 'centroid'], zones.loc[zone[1], 'centroid']]
+        p1 = zones.loc[zone[0], 'centroid']
+        p2 = zones.loc[zone[1], 'centroid']
+        coordinates = [p1, p2]
         folium.PolyLine(locations=coordinates,
                         tooltip=f"PT Volume:{volume:.3f}", 
                         weight=volume/5000,
                         opacity=0.5,
                         color = 'green').add_to(ptFg)
+        if show_text_threshold and volume > show_text_threshold:
+            mid_lat = (p1[0] + p2[0]) / 2
+            mid_lon = (p1[1] + p2[1]) / 2
+            
+            folium.map.Marker(
+                [mid_lat, mid_lon],
+                icon=DivIcon(
+                    icon_size=(180,50),
+                    icon_anchor=(0,0),
+                    html=f'<div style="font-size: 10pt; font-weight: bold; color: darkgreen; background-color: rgba(255,255,255,0.6); padding: 2px; border-radius: 3px; display: inline-block;">{int(volume)}</div>'
+                )
+            ).add_to(ptFg)
     ptFg.add_to(m)
 
     # Adding layer control on the map
